2a16170c76e48e80d0819225ead1be19d8ed529e
capossele <angelocapossele@gmail.com>
2020-08-11 14:58:54 +0100

    ♻️Refactor demo

--- demo/data/client-script.sh
+++ demo/data/client-script.sh
@@ -4,29 +4,10 @@
 IP_ADDR=`ip a | grep global | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -n 1`
 IP_ADDR_PORT="${IP_ADDR}:8000"
 
-# SHARED_FOLDER="/data"
-# PUBLIC_KEY_FILE="${SHARED_FOLDER}/${PORT}.public"
-# GROUP_FILE="${SHARED_FOLDER}/group.toml"
-
-# Generate key pair, add it to the shared file /data/group.toml
+# Generate key pair
 echo "Generating key pair..."
 rm -rf /root/.drand/
 drand generate-keypair --tls-disable "${IP_ADDR_PORT}"
-# cp /root/.drand/key/drand_id.public "${PUBLIC_KEY_FILE}"
-# chmod ugo+rwx "${PUBLIC_KEY_FILE}"
-
-# PUBLIC_KEY=`cat /root/.drand/key/drand_id.public`
-# # echo -en "[[Nodes]]\n${PUBLIC_KEY}\n\n" >> "${GROUP_FILE}"
-# # echo
-
-# # On MacOS, you can't ping/curl the container from the host. Here's a fix
-# apk add curl
-# cat << FIX_SCRIPT > /bin/call_api
-# #!/bin/sh
-# echo "Running curl -s ${IP_ADDR_PORT}/api/public from inside the container:"
-# curl -s ${IP_ADDR_PORT}/api/public
-# FIX_SCRIPT
-# chmod ug+x /bin/call_api
 
 # Boot the drand deamon in background
 nohup drand start --tls-disable --goshimmerAPIurl "$GOSHIMMER" & # add "--verbose 2" here for more details
@@ -36,16 +17,9 @@ sleep 5
 
 # Now nodes wait for the leader to run DKG; leader starts DKG
 if [[ "$LEADER" == 1 ]]; then
-    # sleep 5
-    # echo "We are the leader, checking group..."
-    # drand check-group "${GROUP_FILE}"
-    # echo
-
     echo "Running DKG..."
-    # drand share --leader "${GROUP_FILE}"
     drand share --leader --nodes 5 --threshold 3 --secret "0Q0rRqhUX99nn4SoYME90McZKk+MNtx0OLT5/HTk1tE=" --period 10s
 else
-    # drand share "${GROUP_FILE}"
     sleep 5
     drand share --connect "drand1:8000" --tls-disable --nodes 5 --threshold 3 --secret "0Q0rRqhUX99nn4SoYME90McZKk+MNtx0OLT5/HTk1tE="
 fi
--- demo/run.sh
+++ demo/run.sh
@@ -8,15 +8,21 @@ cleanup () {
 }
 trap 'cleanup ; printf "Tests have been killed via signal.\n"' HUP INT QUIT PIPE TERM
 
-# nodes write in this shared file to build the common group
-GROUP_FILE="./data/group.toml"
-
-# threshold: 3 nodes out of 5, period for randomness: 10 seconds
-# echo -en "Threshold = 3\nPeriod = \"10s\"\n\n" > "${GROUP_FILE}" 
-# chmod ugo+rwx "${GROUP_FILE}" 
-rm -f data/*.public
-
 # build and run 5 docker images (what each container does is in data/client-script.sh)
+docker-compose up -d
+
+COLLECTIVE_PUB_KEY=""
+
+docker stop -t 60 peer_master
+
+while [ -z "$COLLECTIVE_PUB_KEY" ]
+do
+    COLLECTIVE_PUB_KEY=$(docker exec drand1 drand show chain-info | grep public_key | cut -d'"' -f 4)
+    sleep 2
+done
+
+echo "DISTRIBUTED_PUB_KEY=${COLLECTIVE_PUB_KEY}" > .env
+
 docker-compose up -d
 
 echo ""
